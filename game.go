cc02b3b8f60ef612c8ea5ed2f0db1f8b74c3fe93
David W Martines <davidwmartines@gmail.com>
2019-08-26 16:39:57 -0500

    Add initial program stub.

---
+++ game.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+const size int = 3
+
+const playerChar string = "X"
+const compChar string = "O"
+
+type square struct {
+	row int
+	col int
+	val string
+}
+
+var board [size][size]*square
+var boardMap map[string]*square
+
+var winner = false
+
+func main() {
+
+	initializeBoard()
+	drawBoard()
+
+	for !winner {
+		playerGo()
+		computerGo()
+		drawBoard()
+		checkWinner()
+	}
+}
+
+func playerGo() {
+
+	var input string
+	fmt.Println("pick a square: ")
+	fmt.Scanln(&input)
+	if sq, valid := boardMap[input]; valid {
+		sq.val = playerChar
+	} else {
+		fmt.Println("?")
+		playerGo()
+	}
+}
+
+func computerGo() {
+
+}
+
+func checkWinner() {
+
+}
+
+func initializeBoard() {
+	boardMap = make(map[string]*square)
+	number := 0
+	for row := 0; row < size; row++ {
+		for col := 0; col < size; col++ {
+			number++
+			numberVal := strconv.Itoa(number)
+			sq := square{row, col, numberVal}
+			board[row][col] = &sq
+			boardMap[numberVal] = &sq
+		}
+	}
+}
+
+func drawBoard() {
+	for row := 0; row < size; row++ {
+		rowText := ""
+		for col := 0; col < size; col++ {
+			rowText += " " + board[row][col].val
+		}
+		fmt.Println(rowText)
+	}
+}
